9555061097508f6575a337e36ba9e025b4af3c13
ArekWisniewski <158929@student.uwm.edu.pl>
2022-12-05 18:35:12 +0100

    commit1

---
+++ GraSamochodowa2D/Assets/Scripts/RoadLoop.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class RoadLoop : MonoBehaviour {
+
+    public float scrollSpeed;
+    private Vector2 offset;
+
+    void Update()
+    {
+        offset = new Vector2(0, Time.time * scrollSpeed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
